53e7cba5f95d18bad19c8010fab6124dea0e729c
Your Name <you@example.com>
2017-11-24 15:01:07 +0100

    Interface update 1

--- src/app/NewsItem/NewsItem.component.ts
+++ src/app/NewsItem/NewsItem.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { NewsItemService } from './NewsItem.service';
+import { DatePipe } from '@angular/common';
+import { NgClass } from '@angular/common';
 import 'rxjs/add/operator/toPromise';
 @Component({
 	selector: 'app-NewsItem',
